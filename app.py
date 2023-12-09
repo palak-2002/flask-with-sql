7961de6f916d980e6b2ad5623951271d3fc0787a
palak-2002 <83309477+palak-2002@users.noreply.github.com>
2023-12-09 20:55:09 +0000

    added dynamic data

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+Job=[
+  {
+    "id": 5603,
+    "title":"Data Analyst",
+    "loc": "banglore" ,
+    "sal": "12,00,000",
+    "exp": "minimum 1 year"
+  },
+  {
+    "id": 5610,
+    "title": "Data Scientist",
+    "loc": "delhi",
+    "sal": "15,00,000",
+    "exp": "minimum 2 years"
+  },
+  {
+    "id": 5607,
+    "title": "Associate Software Engineer",
+    "loc": "noida",
+    "sal": "8,00,000",
+    "exp": "none"
+  }
+  
+]
+@app.route("/")
+def hello():
+  return render_template("home.html",jobs=Job)
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0',debug=True)
