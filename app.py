8a89c5e65f796c38154567ee8055b7f3ef817cd8
palak-2002 <83309477+palak-2002@users.noreply.github.com>
2023-12-16 13:13:44 +0000

    after database

--- app.py
+++ app.py
@@ -1,37 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
+from database import load_jobs,load_one_job
+
+
 
 app=Flask(__name__)
 
-Job=[
-  {
-    "id": 5603,
-    "title":"Data Analyst",
-    "loc": "banglore" ,
-    "sal": "12,00,000",
-    "exp": "minimum 1 year",
-    "mail":"palakjain2002@gmail.com"
-  },
-  {
-    "id": 5610,
-    "title": "Data Scientist",
-    "loc": "delhi",
-    "sal": "15,00,000",
-    "exp": "minimum 2 years",
-    "mail":"palakjain2002@gmail.com"
-  },
-  {
-    "id": 5607,
-    "title": "Associate Software Engineer",
-    "loc": "noida",
-    "sal": "8,00,000",
-    "exp": "none",
-    "mail":"palakjain2002@gmail.com"
-  }
+# Job=[
+#   {
+#     "id": 5603,
+#     "title":"Data Analyst",
+#     "loc": "banglore" ,
+#     "sal": "12,00,000",
+#     "exp": "minimum 1 year",
+#     "mail":"palakjain2002@gmail.com"
+#   },
+#   {
+#     "id": 5610,
+#     "title": "Data Scientist",
+#     "loc": "delhi",
+#     "sal": "15,00,000",
+#     "exp": "minimum 2 years",
+#     "mail":"palakjain2002@gmail.com"
+#   },
+#   {
+#     "id": 5607,
+#     "title": "Associate Software Engineer",
+#     "loc": "noida",
+#     "sal": "8,00,000",
+#     "exp": "none",
+#     "mail":"palakjain2002@gmail.com"
+#   }
   
-]
+# ]
+
+
+
+  # print(result_dicts)
+
+  
 @app.route("/")
 def hello():
-  return render_template("home.html",jobs=Job)
+  jobs2=load_jobs()
+  return render_template("home.html",jobs=jobs2)
+@app.route("/job/<id>")
+def show_job(id):
+  job=load_one_job(id)
+  if not job:
+    return "Not Found",404
+  return render_template("jobpage.html",job=job)
+  
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
---
+++ database.py
@@ -0,0 +1,42 @@
+from sqlalchemy import create_engine, text
+
+
+db_connection_string="mysql+pymysql://8zeeg975j44gydrnztck:pscale_pw_rRxEYSGMS8ibWb3l7tyBPuBncjmNyMPhdrBzvcAfovI@aws.connect.psdb.cloud/jobsnmore?charset=utf8mb4"
+engine = create_engine(db_connection_string,
+  connect_args={
+     "ssl": {
+             "ssl_ca": "/etc/ssl/cert.pem" } 
+  })
+
+
+
+def load_jobs():
+  with engine.connect() as conn:
+       result=conn.execute(text(
+         "select * from job "))
+
+       jobs1=[]
+       for row in result.all():
+             jobs1.append(row._asdict)
+       return jobs1
+    
+def load_one_job(id):
+  with engine.connect() as conn:
+       result=conn.execute(
+         text("select * from jobs where id=:val"))
+         val=id
+         rows=result.all()
+         if len(rows)==0:
+           return None
+         else:
+           return rows[0]._asdict
+
+# 
+#      result=conn.execute(text("select * from jobs"))
+
+  
+#      result_dicts=[]
+#      for row in result.all():
+#            result_dicts.append(row._asdict)
+       
+# print(result_dicts)
